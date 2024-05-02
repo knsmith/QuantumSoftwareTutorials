a62967839e1764db196d3efdeb3b72a1454c9777
unknown <pchyrchala123@gmail.com>
2024-05-01 21:16:06 -0500

    Got permission for hybrid jobs, and QPU task finally finished.

--- test.py
+++
@@ -1,9 +0,0 @@
-from braket.aws import AwsQuantumJob
-from braket.devices import Devices
-
-job = AwsQuantumJob.create(
-    Devices.Amazon.SV1,
-    source_module="algorithm_script.py",
-    entry_point="algorithm_script:start_here",
-    wait_until_complete=True
-)
